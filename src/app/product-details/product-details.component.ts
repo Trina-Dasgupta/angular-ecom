2fa6cac8d9c6e7e5ced97ce521ec3051fd28b19f
Snehashis Dasgupta <trinadasgupta98@gmail.com>
2023-08-14 00:18:21 +0530

    'added'

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from '../services/product.service';
-import { product } from '../data-type';
+import { cart, product } from '../data-type';
 
 @Component({
   selector: 'app-product-details',
@@ -18,6 +18,16 @@ export class ProductDetailsComponent implements OnInit{
    let productId=this.activeRoute.snapshot.paramMap.get('productId');
   productId && this.product.getProduct(productId).subscribe((result)=>{
     this.productData=result;
+    let cartData=localStorage.getItem('localCart');
+    if(productId && cartData){
+      let items=JSON.parse(cartData);
+      items=items.filter((item:product)=>productId===item.id.toString());
+      if(items.length){
+        this.removeCart=true;
+      }else{
+        this.removeCart=false;
+      }
+    }
   })
  }
  handleQuantity(val:string){
@@ -28,9 +38,32 @@ export class ProductDetailsComponent implements OnInit{
   }
  }
  addToCart(){
-
- }
- removeToCart(id:number){
-
+  if(this.productData){
+    this.productData.quantity = this.productQuantity;
+    if(!localStorage.getItem('user')){
+      this.product.localAddToCart(this.productData);
+      this.removeCart=true
+    }else{
+      let user = localStorage.getItem('user');
+      let userId= user && JSON.parse(user).id;
+      let cartData:cart={
+        ...this.productData,
+        productId:this.productData.id,
+        userId
+      }
+      delete cartData.id;
+      this.product.addToCart(cartData).subscribe((result)=>{
+        if(result){
+         this.product.getCartList(userId);
+         this.removeCart=true
+        }
+      })        
+    }
+    
+  } 
+}
+ removeToCart(productId:number){
+  this.product.removeItemFromCart(productId);
+  this.removeCart=false;
  }
 }
